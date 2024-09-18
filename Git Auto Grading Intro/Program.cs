bdc8a2d727459ea1680d8c8088afb8aac8dca28f
jake22789 <70212338+jake22789@users.noreply.github.com>
2024-09-17 22:46:36 -0600

    calculator finished

--- Git Auto Grading Intro/Program.cs
+++ Git Auto Grading Intro/Program.cs
@@ -9,28 +9,35 @@
             // Call the methods here ex.
             int result = Add(2, 3);
             Console.WriteLine("The result of adding 2 and 3 is: " + result);
+            result = Subtract(5, 3);
+            Console.WriteLine("The result of subtracting 5 and 3 is: " + result);
+            result = Multiply(2, 3);
+            Console.WriteLine("The result of Multipling 2 and 3 is: " + result);
+            result = (int)Divide(6, 3);
+            Console.WriteLine("The result of Dividing 2 and 3 is: " + result);
         }
 
         // Methods Here
         public static int Add(int a, int b)
         {
             // Code
-            return 0; // Placeholder
+            return a+b; // Placeholder
+
         }
         public static int Subtract(int a, int b)
         {
             // Code
-            return 0; // Placeholder
+            return a - b; // Placeholder
         }
         public static int Multiply(int a, int b)
         {
             // Code
-            return 0; // Placeholder
+            return a*b; // Placeholder
         }
         public static decimal Divide(int a, int b)
         {
             // Code
-            return 0; // Placeholder
+            return a/b; // Placeholder
         }
     }
 }
